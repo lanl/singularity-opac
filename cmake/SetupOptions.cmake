252783d6236310dbfc4deb5d47705663d6515a60
Christopher Mauney <mauneyc@lanl.gov>
2021-06-22 10:50:20 -0600

    initial kokkos logic

--- cmake/SetupOptions.cmake
+++ cmake/SetupOptions.cmake
@@ -16,7 +16,7 @@ option (SINGULARITY_BETTER_DEBUG_FLAGS
 # Dependency options
 #=======================================
 # check for using in-tree third-party dependencies
-option (SINULARITY_IN_TREE "Use in-tree dependencies" ON)
+# option (SINULARITY_KOKKOS_IN_TREE "Use in-tree dependencies" ON)
 
 cmake_dependent_option (SINGULARITY_USE_KOKKOS "Use Kokkos for portability" ON "Kokkos_FOUND" OFF)
 cmake_dependent_option (SINGULARITY_USE_CUDA "Enable cuda support" ON "SINGULARITY_USE_KOKKOS" OFF)
@@ -26,6 +26,19 @@ cmake_dependent_option (SINGULARITY_USE_HDF5 "Pull in hdf5" ON "HDF5_FOUND" OFF)
 # Options logic
 #=======================================
 # check for currently incompatible option
-if(SINGULARITY_USE_CUDA AND NOT SINGULARITY_USE_KOKKOS)
-  message(FATAL_ERROR "Cuda without kokkos is not currently supported")
+if(SINGULARITY_USE_CUDA)
+  if(SINGULARITY_USE_KOKKOS)
+    if(NOT Kokkos_FOUND)
+      message(STATUS "Kokkos requested, but not found. Configuring internal submodule.")
+      set(Kokkos_ENABLE_CUDA ON CACHE BOOL "" FORCE)
+      set(Kokkos_ENABLE_SERIAL ON CACHE BOOL "" FORCE)
+      set(Kokkos_ENABLE_CUDA_LAMBDA ON CACHE BOOL "" FORCE)
+      set(Kokkos_ENABLE_CUDA_RELOCATABLE_DEVICE_CODE ON CACHE BOOL "" FORCE)
+      add_subdirectory(${PROJECT_SOURCE_DIR}/utils/kokkos)
+    endif()
+  endif()
+  else()
+    message(FATAL_ERROR "Cuda without kokkos is not currently supported")
+  endif()
+
 endif()
