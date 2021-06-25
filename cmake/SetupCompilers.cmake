1af992b0656f0bfc1c4a8e06040831993bc0d188
Christopher Mauney <mauneyc@lanl.gov>
2021-06-25 10:17:35 -0600

    removed commented code

--- cmake/SetupCompilers.cmake
+++ cmake/SetupCompilers.cmake
@@ -2,21 +2,6 @@
 # Compiler logic
 #=======================================
 
-#TODO: Do CMake CUDA options help here?
-#if(SINGULARITY_USE_KOKKOS AND SINGULARITY_USE_CUDA)
-# set nvcc wrapper default compiler
-#  if(NOT "$ENV{CXX}x" STREQUAL "x" AND
-#     "$ENV{NVCC_WRAPPER_DEFAULT_COMPILER}x" STREQUAL "x")
-#    set(ENV{NVCC_WRAPPER_DEFAULT_COMPILER} "$ENV{CXX}")
-#  endif()
-  # set necessary kokkos build options if building inline
-#  if(NOT Kokkos_FOUND)
-#    set(NVCC_WRAPPER_DIR ${CMAKE_CURRENT_SOURCE_DIR}/utils/kokkos/bin CACHE STRING "")
-#  endif()
-
- # set(CMAKE_CXX_COMPILER ${KOKKOS_CXX_COMPILER} CACHE STRING "")
-#endif()
-
 # set C++ options
 enable_language(CXX)
 include(CMakeDetermineCXXCompiler)
