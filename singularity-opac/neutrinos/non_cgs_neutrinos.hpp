a9a306866e302f6356019921ba5e4840d4b0f9cb
Benjamin Ransom Ryan <brryan@lanl.gov>
2022-04-12 14:25:18 -0600

    Forgot some unit conversions

--- singularity-opac/neutrinos/non_cgs_neutrinos.hpp
+++ singularity-opac/neutrinos/non_cgs_neutrinos.hpp
@@ -183,21 +183,23 @@ class NonCGSUnits {
   PORTABLE_INLINE_FUNCTION
   Real ThermalDistributionOfTNu(const Real temp, const RadiationType type,
                                 const Real nu, Real *lambda = nullptr) const {
-    Real BoH = opac_.ThermalDistributionOfTNu(temp, type, nu, lambda);
+    Real BoH = opac_.ThermalDistributionOfTNu(temp * temp_unit_, type,
+                                              nu * freq_unit_, lambda);
     return BoH * inv_intensity_unit_;
   }
 
   PORTABLE_INLINE_FUNCTION
   Real ThermalDistributionOfT(const Real temp, const RadiationType type,
                               Real *lambda = nullptr) const {
-    Real BoH = opac_.ThermalDistributionOfT(temp, type, lambda);
+    Real BoH = opac_.ThermalDistributionOfT(temp * temp_unit_, type, lambda);
     return BoH * inv_energy_dens_unit_;
   }
 
   PORTABLE_INLINE_FUNCTION
   Real ThermalNumberDistributionOfT(const Real temp, const RadiationType type,
                                     Real *lambda = nullptr) const {
-    Real NoH = opac_.ThermalNumberDistributionOfT(temp, type, lambda);
+    Real NoH =
+        opac_.ThermalNumberDistributionOfT(temp * temp_unit_, type, lambda);
     return NoH * mass_unit_ / rho_unit_;
   }
 
